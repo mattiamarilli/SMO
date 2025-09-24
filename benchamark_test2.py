ef790bfc36589ef45e5a22417a7abba76878f816
mattiamarilli <mattia.marilli@madeinapp.net>
2025-09-24 12:20:45 +0200

    wip 2

--- benchamark_test2.py
+++ benchamark_test2.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from test2 import SVM
 
-def benchmark_svm(num_samples=1000, num_features=10, test_size=0.2, random_state=42):
+def benchmark_svm(num_samples=2000, num_features=120, test_size=0.2, random_state=42):
     X, y = make_classification(n_samples=num_samples, n_features=num_features, n_informative=int(num_features * 0.6),
         n_redundant=int(num_features * 0.1), n_classes=2, n_clusters_per_class=1, random_state=random_state)
 
@@ -22,8 +22,9 @@ def benchmark_svm(num_samples=1000, num_features=10, test_size=0.2, random_state
 
     gamma = 1.0 / num_features
 
-    model = SVM(c=1.0,kkt_thr=1e-3, max_iter=1000, kernel_type='rbf', gamma_rbf=gamma)
+    model = SVM(c=1.0,kkt_thr=1e-3, max_iter=1200, kernel_type='rbf', gamma_rbf=gamma)
     start_time = time.time()
+    print("Hello")
     model.fit(X_train, y_train)
     training_time = time.time() - start_time
     print(f"Training time (SVM personalizzato): {training_time:.3f} seconds")
--- test2.py
+++ test2.py
@@ -1,47 +1,54 @@
 from typing import Tuple
 import numpy as np
+import time
+
 
 class SVM:
     def __init__(self, c: float = 1., kkt_thr: float = 1e-3, max_iter: int = 1e4, kernel_type: str = 'linear', gamma_rbf: float = 1.) -> None:
-        if not kernel_type in ['linear', 'rbf']:
+        if kernel_type not in ['linear', 'rbf']:
             raise ValueError('kernel_type must be either {} or {}'.format('linear', 'rbf'))
 
         super().__init__()
 
-        # Initialize
         self.c = float(c)
-        self.max_iter = max_iter
+        self.max_iter = int(max_iter)
         self.kkt_thr = kkt_thr
+
         if kernel_type == 'linear':
             self.kernel = self.linear_kernel
         elif kernel_type == 'rbf':
             self.kernel = self.rbf_kernel
             self.gamma_rbf = gamma_rbf
 
-        self.b = np.array([])
+        self.b = 0.0
         self.alpha = np.array([])
         self.support_vectors = np.array([])
         self.support_labels = np.array([])
 
+    # ------------------- PREDICT -------------------
     def predict(self, x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        w = self.support_labels * self.alpha
-        x = self.kernel(self.support_vectors, x)
-
-        scores = np.matmul(w, x) + self.b
+        if self.alpha.shape[0] == 0:
+            raise ValueError("Model not trained yet.")
+        K_test = self.kernel(self.support_vectors, x)
+        scores = np.dot(self.alpha * self.support_labels, K_test) + self.b
         pred = np.sign(scores)
-
         return pred, scores
 
+    # ------------------- FIT -------------------
     def fit(self, x_train: np.ndarray, y_train: np.ndarray) -> None:
         N, D = x_train.shape
-        self.b = 0
+        self.b = 0.0
         self.alpha = np.zeros(N)
         self.support_labels = y_train
         self.support_vectors = x_train
+        self.sumtimes = 0
 
         iter_idx = 0
         non_kkt_array = np.arange(N)
-        error_cache = self.predict(x_train)[1] - y_train
+        #start_time_cache = time.time()
+        #error_cache = self.predict(x_train)[1] - y_train
+        #print(f"Start time (SVM personalizzato): {time.time() -start_time_cache} seconds")
+        error_cache = -np.ones(N)
 
         print("SVM training using SMO algorithm - START")
         while iter_idx < self.max_iter:
@@ -50,56 +57,77 @@ class SVM:
                 break
 
             i_1 = self.i1_heuristic(i_2, error_cache)
-
             if i_1 == i_2:
                 continue
 
-            x_1, y_1, alpha_1 = self.support_vectors[i_1, :], self.support_labels[i_1], self.alpha[i_1]
-            x_2, y_2, alpha_2 = self.support_vectors[i_2, :], self.support_labels[i_2], self.alpha[i_2]
+            y_1, alpha_1 = self.support_labels[i_1], self.alpha[i_1]
+            y_2, alpha_2 = self.support_labels[i_2], self.alpha[i_2]
 
+            # --- Precalcolo colonne kernel ---
+            start_time = time.time()
+            K_i1 = self.kernel_column(i_1)
+            K_i2 = self.kernel_column(i_2)
+            self.sumtimes += time.time() - start_time
+            k11 = K_i1[i_1]
+            k22 = K_i2[i_2]
+            k12 = K_i1[i_2]
+
+            # --- Calcolo boundaries ---
             L, H = self.compute_boundaries(alpha_1, alpha_2, y_1, y_2)
             if L == H:
                 continue
 
-            eta = self.compute_eta(x_1, x_2)
+            # --- Calcolo eta ---
+            eta = k11 + k22 - 2 * k12
             if eta == 0:
                 continue
 
-            _, score_1 = self.predict(x_1)
-            _, score_2 = self.predict(x_2)
+            # --- Calcolo errori ---
+            E_1 = np.dot(self.alpha * self.support_labels, K_i1) + self.b - y_1
+            E_2 = np.dot(self.alpha * self.support_labels, K_i2) + self.b - y_2
 
-            E_1 = score_1 - y_1
-            E_2 = score_2 - y_2
-
+            # --- Aggiornamento alpha ---
             alpha_2_new = alpha_2 + y_2 * (E_1 - E_2) / eta
-            alpha_2_new = np.minimum(alpha_2_new, H)
-            alpha_2_new = np.maximum(alpha_2_new, L)
-
+            alpha_2_new = np.clip(alpha_2_new, L, H)
             alpha_1_new = alpha_1 + y_1 * y_2 * (alpha_2 - alpha_2_new)
 
-            self.compute_b(alpha_1_new, alpha_2_new, E_1, E_2, i_1, i_2)
+            # --- Aggiornamento b ---
+            b1 = self.b - E_1 - y_1 * (alpha_1_new - alpha_1) * k11 - y_2 * (alpha_2_new - alpha_2) * k12
+            b2 = self.b - E_2 - y_1 * (alpha_1_new - alpha_1) * k12 - y_2 * (alpha_2_new - alpha_2) * k22
 
+            if 0 < alpha_1_new < self.c:
+                self.b = b1
+            elif 0 < alpha_2_new < self.c:
+                self.b = b2
+            else:
+                self.b = (b1 + b2) / 2
+
+            # --- Salvataggio alpha aggiornati ---
             self.alpha[i_1] = alpha_1_new
             self.alpha[i_2] = alpha_2_new
 
-            error_cache[i_1] = self.predict(x_1)[1] - y_1
-            error_cache[i_2] = self.predict(x_2)[1] - y_2
+            # --- Aggiornamento cache errori ---
+            error_cache[i_1] = np.dot(self.alpha * self.support_labels, K_i1) + self.b - y_1
+            error_cache[i_2] = np.dot(self.alpha * self.support_labels, K_i2) + self.b - y_2
 
-            iter_idx = iter_idx + 1
+            iter_idx += 1
 
+        # --- Filtraggio support vectors ---
         support_vectors_idx = (self.alpha != 0)
         self.support_labels = self.support_labels[support_vectors_idx]
         self.support_vectors = self.support_vectors[support_vectors_idx, :]
         self.alpha = self.alpha[support_vectors_idx]
 
-        print(f"Training summary: {iter_idx} iterations, {self.alpha.shape[0]} supprts vectors")
+        print(f"Training summary: {iter_idx} iterations, {self.alpha.shape[0]} support vectors")
         print("SVM training using SMO algorithm - DONE!")
+        print(f"Tempo totale calcolo colonne: {self.sumtimes}")
 
+    # ------------------- HEURISTICS -------------------
     def i1_heuristic(self, i_2, error_cache) -> int:
         E_2 = error_cache[i_2]
-        non_bounded_idx = np.argwhere((0 < self.alpha) & (self.alpha < self.c)).reshape((1, -1))[0]
+        non_bounded_idx = np.argwhere((0 < self.alpha) & (self.alpha < self.c)).reshape(-1)
 
-        if non_bounded_idx.shape[0] > 0:
+        if non_bounded_idx.size > 0:
             if E_2 >= 0:
                 i_1 = non_bounded_idx[np.argmin(error_cache[non_bounded_idx])]
             else:
@@ -114,7 +142,6 @@ class SVM:
 
         for idx in non_kkt_array:
             non_kkt_array = np.delete(non_kkt_array, np.argwhere(non_kkt_array == idx))
-
             if not self.check_kkt(idx):
                 i_2 = idx
                 break
@@ -122,13 +149,14 @@ class SVM:
         if i_2 == -1:
             idx_array = np.arange(self.alpha.shape[0])
             non_kkt_array = idx_array[~(self.check_kkt(idx_array))]
-            if non_kkt_array.shape[0] > 0:
+            if non_kkt_array.size > 0:
                 np.random.shuffle(non_kkt_array)
                 i_2 = non_kkt_array[0]
                 non_kkt_array = non_kkt_array[1:-1]
 
         return i_2, non_kkt_array
 
+    # ------------------- KKT CHECK -------------------
     def check_kkt(self, check_idx: int) -> np.ndarray:
         alpha_idx = self.alpha[check_idx]
         _, score_idx = self.predict(self.support_vectors[check_idx, :])
@@ -136,50 +164,33 @@ class SVM:
         r_idx = y_idx * score_idx - 1
         cond_1 = (alpha_idx < self.c) & (r_idx < - self.kkt_thr)
         cond_2 = (alpha_idx > 0) & (r_idx > self.kkt_thr)
-
         return ~(cond_1 | cond_2)
 
+    # ------------------- BOUNDS -------------------
     def compute_boundaries(self, alpha_1, alpha_2, y_1, y_2) -> Tuple[float, float]:
         if y_1 == y_2:
-            lb = np.max([0, alpha_1 + alpha_2 - self.c])
-            ub = np.min([self.c, alpha_1 + alpha_2])
+            lb = max(0, alpha_1 + alpha_2 - self.c)
+            ub = min(self.c, alpha_1 + alpha_2)
         else:
-            lb = np.max([0, alpha_2 - alpha_1])
-            ub = np.min([self.c, self.c + alpha_2 - alpha_1])
+            lb = max(0, alpha_2 - alpha_1)
+            ub = min(self.c, self.c + alpha_2 - alpha_1)
         return lb, ub
 
-    def compute_eta(self, x_1, x_2) -> float:
-        return self.kernel(x_1, x_1) + self.kernel(x_2, x_2) - 2 * self.kernel(x_1, x_2)
+    # ------------------- KERNEL COLUMN -------------------
+    def kernel_column(self, i: int) -> np.ndarray:
+        x_i = self.support_vectors[i]
+        return self.kernel(self.support_vectors, x_i)
 
-    def compute_b(self, alpha_1_new, alpha_2_new, E_1, E_2, i_1, i_2) -> None:
-        x_1 = self.support_vectors[i_1]
-        x_2 = self.support_vectors[i_2]
-
-        b1 = self.b - E_1 - self.support_labels[i_1] * (alpha_1_new - self.alpha[i_1]) * self.kernel(x_1, x_1) - \
-             self.support_labels[i_2] * (alpha_2_new - self.alpha[i_2]) * self.kernel(x_1, x_2)
-
-        b2 = self.b - E_2 - self.support_labels[i_1] * (alpha_1_new - self.alpha[i_1]) * self.kernel(x_1, x_2) - \
-             self.support_labels[i_2] * (alpha_2_new - self.alpha[i_2]) * self.kernel(x_2, x_2)
-
-        if 0 < alpha_1_new < self.c:
-            self.b = b1
-        elif 0 < alpha_2_new < self.c:
-            self.b = b2
-        else:
-            self.b = np.mean([b1, b2])
-
+    # ------------------- KERNELS -------------------
     def rbf_kernel(self, u, v):
         if np.ndim(v) == 1:
             v = v[np.newaxis, :]
-
         if np.ndim(u) == 1:
             u = u[np.newaxis, :]
-
         dist_squared = np.linalg.norm(u[:, :, np.newaxis] - v.T[np.newaxis, :, :], axis=1) ** 2
         dist_squared = np.squeeze(dist_squared)
-
         return np.exp(-self.gamma_rbf * dist_squared)
 
     @staticmethod
-    def linear_kernel(u,v) -> np.ndarray:
+    def linear_kernel(u, v) -> np.ndarray:
         return np.dot(u, v.T)
